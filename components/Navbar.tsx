ec845ee23b224558920ab87899431dc1244a4916
Madhav-000-s <madhavendranaths@gmail.com>
2025-12-03 17:55:23 +0530

    initialising

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { navLinks } from "@/constants"
+
+export default function Navbar(){
+    return (
+        <nav>
+            <div>
+                navbar
+
+                <ul>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
